bd60a55038ea282767689fff46c361ed6ac41e07
thecoderist <delacruzbrian315@gmail.com>
2025-05-14 22:13:37 +0800

    making content for my footer

---
+++ front-techcart/src/components/footer.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const footer = () => {
+  return (
+    <div>  
+         {/* Footer */}
+    <footer className="footer mt-5 text-center bg-black">
+      <p className='text-white'>TechCart © 2025 | Making PC parts and Accessories</p>
+      <p className='text-white'>Follow us for the latest PC components, updates, and PC treats!</p>
+      <a
+        href="https://www.facebook.com/easypc.ph"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="footer-link text-white"
+
+      >
+        TechCart.facebook.com
+      </a>
+    </footer></div>
+  )
+}
+
+export default footer
