5e00a8e42a1a7416439feb24a074420230a5214e
thecoderist <delacruzbrian315@gmail.com>
2025-05-15 01:01:02 +0800

    for cart items database

--- front-techcart/backend-techcart/database/migrations/2025_05_14_165919_create_cart_items_table.php
+++ front-techcart/backend-techcart/database/migrations/2025_05_14_165919_create_cart_items_table.php
@@ -1,27 +1,25 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateCartItemsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('cart_items', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('cart_id')->constrained()->onDelete('cascade');
+            $table->foreignId('product_id')->constrained()->onDelete('cascade');
+            $table->integer('quantity');
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('cart_items');
     }
-};
+}
