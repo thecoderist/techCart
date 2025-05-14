b593b6bcd8cef127285c9ef09a795638e14e894f
thecoderist <delacruzbrian315@gmail.com>
2025-05-15 00:55:47 +0800

    making Order Items

--- front-techcart/backend-techcart/app/Models/Orderitem.php
+++ front-techcart/backend-techcart/app/Models/Orderitem.php
@@ -5,7 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Orderitem extends Model
+class OrderItem extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'order_id',
+        'product_id',
+        'quantity',
+        'price',
+    ];
+
+    public function order()
+    {
+        return $this->belongsTo(Order::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
 }
