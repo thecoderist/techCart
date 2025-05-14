51a3aed6f9d6201d0fff1929ee6d435bfc6ebe4d
thecoderist <delacruzbrian315@gmail.com>
2025-05-15 00:56:06 +0800

    Product model

--- front-techcart/backend-techcart/app/Models/Product.php
+++ front-techcart/backend-techcart/app/Models/Product.php
@@ -7,5 +7,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
+
+    protected $fillable = [
+        'title',
+        'description',
+        'price',
+        'stock',
+        'image',
+    ];
+    
+    public function carts()
+    {
+        return $this->hasMany(Cart::class);
+    }
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+    
     use HasFactory;
 }
