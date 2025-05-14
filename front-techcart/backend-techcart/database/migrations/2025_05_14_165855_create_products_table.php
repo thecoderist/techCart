112f04a86e51867a332b92caefeeaf2359e4b241
thecoderist <delacruzbrian315@gmail.com>
2025-05-15 01:00:20 +0800

    Modification for create product table

--- front-techcart/backend-techcart/database/migrations/2025_05_14_165855_create_products_table.php
+++ front-techcart/backend-techcart/database/migrations/2025_05_14_165855_create_products_table.php
@@ -4,24 +4,23 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateProductsTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->decimal('price', 10, 2);
+            $table->integer('stock');
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('products');
     }
-};
+}
