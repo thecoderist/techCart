968ce3ca919c0a7e54e83dcb19591c6cd0bf2d84
thecoderist <delacruzbrian315@gmail.com>
2025-05-15 00:55:25 +0800

    making order model

--- front-techcart/backend-techcart/app/Models/Order.php
+++ front-techcart/backend-techcart/app/Models/Order.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'total',
+        'customer_name',
+        'customer_email',
+        'customer_address',
+        'customer_contact',
+        'customer_birthday',
+        'customer_gender',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
