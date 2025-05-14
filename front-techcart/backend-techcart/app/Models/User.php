3114ce34d1988dd3f3e688203b49f5c498a4e135
thecoderist <delacruzbrian315@gmail.com>
2025-05-15 00:56:26 +0800

    modifications for users

--- front-techcart/backend-techcart/app/Models/User.php
+++ front-techcart/backend-techcart/app/Models/User.php
@@ -1,8 +1,6 @@
 <?php
 
 namespace App\Models;
-
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -18,11 +16,16 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'first_name',
+        'last_name',
+        'birthday',
+        'gender',
+        'address',
+        'contact_number',
         'email',
         'password',
+        'role',
     ];
-
     /**
      * The attributes that should be hidden for serialization.
      *
@@ -32,7 +35,15 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+    public function carts()
+    {
+        return $this->hasOne(Cart::class);
+    }
 
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
     /**
      * The attributes that should be cast.
      *
@@ -41,4 +52,6 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+
 }
